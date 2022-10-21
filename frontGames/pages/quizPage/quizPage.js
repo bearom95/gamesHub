8f98e2f2b75219b781e480be6776883b2c7b7122
bearom95 <bearom95@gmail.com>
2022-10-21 20:48:04 +0200

    quiz funcionando

--- frontGames/pages/quizPage/quizPage.js
+++ frontGames/pages/quizPage/quizPage.js
@@ -1,6 +1,7 @@
-//importar sus estilos
+import "./quizPage.css";
 import { allQuestions } from "../../quizQuestions";
 import { cleanPage } from "../../utils/cleanpage";
+import { callBtnMenu } from "../../components/menuButton";
 
 let score = 0;
 let pos = 0;
@@ -8,38 +9,56 @@ let pos = 0;
 export const getQuiz = () => {
   let maindiv = document.querySelector(".maindiv");
   cleanPage(maindiv);
+  callBtnMenu();
   maindiv.innerHTML = `
   <h1>Quiz Game</h1>
-  <span class="score">0</span>`;
-  printCheckQuestion(allQuestions, pos);
+  <button id="startQuizBtn">Start</button>`;
+  const startQuizBtn = document.querySelector("#startQuizBtn");
+  startQuizBtn.addEventListener("click", () =>
+    printCheckQuestion(allQuestions, pos)
+  );
 };
 
-const checkAnswer = (objeto, valor) => {
+//<span class="score">0</span> si quisiera incluir el contador al principio
+
+const checkAnswer = (objeto, clave) => {
   /* console.log(pos); */
   if (pos == allQuestions.length - 1) {
-    console.log("se acabó");
+    const endDiv = document.createElement("div");
+    let maindiv = document.querySelector(".maindiv");
+    cleanPage(maindiv);
+    maindiv.appendChild(endDiv);
+    endDiv.innerHTML = `
+    <p>You're done, your total score is ${score} out of 10</p>
+    <button id="retakeBtn">Retake Quiz</button>
+    `;
+    pos = 0;
+    score = 0;
+    const retakeBtn = document.querySelector("#retakeBtn");
+    retakeBtn.addEventListener("click", () =>
+      printCheckQuestion(allQuestions, pos)
+    );
+  } else if (objeto[clave] == true) {
+    score++;
+    pos += 1;
+    console.log("score" + score);
+    printCheckQuestion(allQuestions, pos);
   } else {
-    if (objeto[valor] == true) {
-      score++;
-      pos += 1;
-      console.log("score" + score);
-      printCheckQuestion(allQuestions, pos);
-    } else {
-      pos += 1;
-      console.log(score);
-      printCheckQuestion(allQuestions, pos);
-    }
+    pos += 1;
+    console.log(score);
+    printCheckQuestion(allQuestions, pos);
   }
 };
 
 const printCheckQuestion = (list, value) => {
   const element = list[value];
   let questionDiv = document.createElement("div");
+  questionDiv.classList.add("questionDiv");
   let maindiv = document.querySelector(".maindiv");
   cleanPage(maindiv);
   maindiv.appendChild(questionDiv);
   questionDiv.innerHTML = `
-    <h2 id="questionSentence">${element.question}</h2>
+    <h2 id="questionSentence">${pos + 1}. ${element.question}</h2>
     `;
 
   for (const key in element.answers) {
@@ -52,5 +71,3 @@ const printCheckQuestion = (list, value) => {
     questionDiv.appendChild(answer);
   }
 };
-
-/* event.target.innerText */
